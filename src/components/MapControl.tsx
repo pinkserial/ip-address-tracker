44a1bb13beb50960befda995194f6c266ae1f5f3
jongmin <pinkserial@gmail.com>
2023-06-22 11:56:30 +0900

    icon 버그 수정

--- src/components/MapControl.tsx
+++ src/components/MapControl.tsx
@@ -2,6 +2,7 @@ import L from "leaflet";
 import { Marker } from "react-leaflet";
 import { useMap } from "react-leaflet/hooks";
 import { Location } from "../types/geoLocation";
+import iconLocation from "/images/icon-location.svg";
 
 const MapControl = ({ location }: { location: Location }) => {
   const parentMap = useMap();
@@ -9,7 +10,7 @@ const MapControl = ({ location }: { location: Location }) => {
 
   parentMap.setView(position, 15);
 
-  const icon = L.icon({ iconUrl: "/public/images/icon-location.svg", iconAnchor: [23, 56]});
+  const icon = L.icon({ iconUrl: iconLocation, iconAnchor: [23, 56] });
 
   return <Marker position={position} icon={icon} />;
 };
